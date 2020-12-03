296d9b1e28bc886e1475510df123364684735757
anetz89 <anetz@gmx.net>
2020-12-03 22:45:17 +0100

    aoc03 why so complex :)

--- src/aoc03/main.ts
+++ src/aoc03/main.ts
@@ -1,35 +1,21 @@
 import { read } from '../shared/importer';
-import { Position } from './position';
-import { SlopeMap } from './slopeMap';
-
 
 export function aoc03 (slopes: number[][]): number {
-    const list: string[] = read('./assets/aoc03.txt');
-    const pattern: string[][] = list.map(col => col.split(''));
+    const pattern: string[] = read('./assets/aoc03.txt');
 
-    const map = new SlopeMap(pattern);
-
-    const slopeTreeCounts = slopes.map((slope): number => getTreeCount(map, slope));
-
-    console.log(slopeTreeCounts);
-
-    return slopeTreeCounts.reduce((a: number, b: number) => a * b);
+    return slopes.map((slope): number => getTreeCount(pattern, slope))
+        .reduce((a: number, b: number) => a * b);
 }
 
-function getTreeCount(map: SlopeMap, slope: number[]) {
-    const positions: Position[] =  slide(map, map.getPosition(0, 0), slope);
+function getTreeCount(pattern: string[], slope: number[]) {
+    let count = 0;
+    let x = 0;
 
-    return positions.filter(position => position.isTree).length;
-}
-
-function slide(map: SlopeMap, position: Position, slope: number[], result: Position[] = []): Position[] {
-    if (position.y >= map.sizeY - 1) {
-        return result;
+    for (let y = 0, sizeY = pattern.length - 1; y <= sizeY - slope[1]; ) {
+        if (pattern[y += slope[1]].charAt((x += slope[0]) % pattern[y].length) === '#') {
+            count += 1;
+        }
     }
 
-    const newPosition = map.slide(position, slope[0], slope[1]);
-
-    result.push(newPosition);
-
-    return slide(map, newPosition, slope, result);
+    return count;
 }
