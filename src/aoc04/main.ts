128504973bf16a2809cd0fea276dc533fa0da391
anetz89 <anetz@gmx.net>
2020-12-04 14:32:16 +0100

    aoc04 minor code cleanup

--- src/aoc04/main.ts
+++ src/aoc04/main.ts
@@ -13,15 +13,18 @@ function isValidPassport(passport: string, enableValueCheck: boolean): boolean {
 
      return entries.filter(entry => {
         const entryVals = entry.split(':');
+        let regex;
+
         switch(entryVals[0]) {
-            case 'byr': return enableValueCheck? /^(19[2-9][0-9]|200[0-2])$/.test(entryVals[1]) : true;
-            case 'iyr': return enableValueCheck? /^(201[0-9]|2020)$/.test(entryVals[1]) : true;
-            case 'eyr': return enableValueCheck? /^(202[0-9]|2030)$/.test(entryVals[1]) : true;
-            case 'hgt': return enableValueCheck? /^(1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)$/.test(entryVals[1]) : true;
-            case 'hcl': return enableValueCheck? /^#[a-f0-9]{6}$/.test(entryVals[1]) : true;
-            case 'ecl': return enableValueCheck? /^(amb|blu|brn|gry|grn|hzl|oth)$/.test(entryVals[1]) : true;
-            case 'pid': return enableValueCheck? /^0*\d{9}$/.test(entryVals[1]) : true;
+            case 'byr': regex = /^(19[2-9][0-9]|200[0-2])$/;        break;
+            case 'iyr': regex = /^(201[0-9]|2020)$/;                break;
+            case 'eyr': regex = /^(202[0-9]|2030)$/;                break;
+            case 'hgt': regex = /^(1[5-8][0-9]cm|19[0-3]cm|59in|6[0-9]in|7[0-6]in)$/; break;
+            case 'hcl': regex = /^#[a-f0-9]{6}$/;                   break;
+            case 'ecl': regex = /^(amb|blu|brn|gry|grn|hzl|oth)$/;  break;
+            case 'pid': regex = /^0*\d{9}$/;                        break;
             default: return false;
         }
+        return enableValueCheck ? regex.test(entryVals[1]) : true;
      }).length === 7;  // warning! only works without duplicate entries
 }
