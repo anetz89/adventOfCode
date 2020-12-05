83266eff4ef99e62f91affe6020e933ee5bd65ae
anetz89 <anetz@gmx.net>
2020-12-05 20:00:04 +0100

    why not use id only?

--- src/aoc05/main.ts
+++ src/aoc05/main.ts
@@ -2,37 +2,17 @@ import  { readStringList } from '../shared/importer';
 
 export function aoc05 (getHighestTicketId = false): any {
     const list: string[] = readStringList('./assets/aoc05.txt');
-    const plane: number[] = [];
-    let maxId = 0;
-
-    list.map(ticket => ticket.replace(/(B|R)/g, '1').replace(/(F|L)/g, '0'))
-        .map(ticket => {
-            const row = parseInt(ticket.slice(0,7), 2);
-            if (!plane[row]) {
-                plane[row] = 0;
-            }
-            plane[row] += parseInt(ticket.slice(7), 2);
-
-            // find highest ticket id
-            const id = parseInt(ticket, 2);
-            if (id > maxId) {
-                maxId = id;
-            }
-        });
+    const plane = list.map(ticket => parseInt(ticket.replace(/(B|R)/g, '1').replace(/(F|L)/g, '0'), 2));
 
     if (getHighestTicketId) {
-        return maxId;
+        return Math.max(...plane);
     }
 
     return findMissingSeat(plane);
 }
 
 function findMissingSeat(plane: number[]): any {
-    for(let i = 0; i < 128; i += 1) {
-        if (plane[i] && plane[i - 1] && plane[i] < 28) {
-            return (i * 8) + 28 - plane[i];
-        }
-    }
-
-    return [0, 0];
+    return plane.sort((a, b) => a - b).filter((place, index) => {
+        return place + 1 !== plane[index + 1];
+    })[0] + 1;
 }
