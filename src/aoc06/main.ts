adf6d5f6d5fd0768943155e99b74f2fcb420fd89
anetz89 <anetz@gmx.net>
2020-12-06 11:09:36 +0100

    aoc06 spread alphabet

--- src/aoc06/main.ts
+++ src/aoc06/main.ts
@@ -7,8 +7,7 @@ export function aoc06 (countExplicit = false): number {
 }
 
 function getSum(block: string, countExplicit: boolean) {
-    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-        'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
     const blocks = block.split('\n');
 
     return alphabet
